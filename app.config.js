1108b55bcaee715c91177533576ab70ff8789ffd
eduardo <eduardo3444@gmail.com>
2025-04-02 23:12:14 -0400

    Cambios agregados para exportación apk.

--- app.config.js
+++ app.config.js
@@ -5,6 +5,10 @@ export default {
     name: "MoodTrack",
     slug: "moodtrack",
     version: "1.0.0",
+    owner: "edu-ugarteche",
+    android: {
+      package: "com.eduugarteche.moodtrack", // 👈 Aquí defines tu package name único
+    },
     extra: {
       firebaseApiKey: process.env.FIREBASE_API_KEY,
       firebaseAuthDomain: process.env.FIREBASE_AUTH_DOMAIN,
@@ -12,6 +16,9 @@ export default {
       firebaseStorageBucket: process.env.FIREBASE_STORAGE_BUCKET,
       firebaseMessagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
       firebaseAppId: process.env.FIREBASE_APP_ID,
+      eas: {
+        projectId: "4b14bdf8-4cad-4ac2-883e-998e3e4103a0",
+      },
     },
   },
 };
